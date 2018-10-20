79658d13633873792ef29e4383dc5d7f3ee249af
JohnnyMel <ioannis.meletiou@gmail.com>
2018-10-20 17:10:35 +0300

    added some stuff

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World 3 !");
+        System.out.println("conflict?");
+        test();
+
+    }
+
+
+    public static void test() {
+        String i = "afasf";
+        int j = 7;
+
+
     }
 }
